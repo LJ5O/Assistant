eb6c6bce60b9a0a37d1898e0258571d942b563b6
LJ5O <75009579+LJ5O@users.noreply.github.com>
2025-06-10 23:28:27 +0200

    Working on login form

---
+++ apps/web-app/src/components/Form/Input.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+const model = defineModel<string>({required: true})
+
+export interface Props{
+    type?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    type: 'text'
+})
+</script>
+
+<template>
+<input
+  v-model="model"
+  :type="props.type"
+  class="
+    rounded-lg px-2 py-1 text-sm whitespace-nowrap outline-none
+    shadow-sm
+    bg-neutral-950 focus:bg-neutral-900
+    border-2 border-neutral-900 focus:border-gray-500
+    transition-all duration-200 ease-in-out
+    text-neutral-50 disabled:text-neutral-600
+    disabled:cursor-not-allowed
+  "
+/>
+</template>
---
+++ apps/web-app/src/views/Login.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+    import {ref} from 'vue'
+
+    import Input from "../components/Form/Input.vue"
+
+    const value = ref('')
+</script>
+
+<template>
+    <div class="w-full h-full flex justify-center content-center flex-wrap flex-col gap-4">
+        <h1 class="text-white w-1/4 min-w-80 text-center text-2xl lg:text-5xl">Application</h1>
+        <div class="
+            w-1/4 h-2/4 min-w-80 min-h-[400px] bg-blue-600 border-2 border-gray-500 rounded-lg shadow-2xl/80 shadow-black
+            flex flex-col content-center text-center
+        ">
+            <p>Login</p>
+            <Input v-model="value" class="w-3/4 mr-auto ml-auto"/>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
