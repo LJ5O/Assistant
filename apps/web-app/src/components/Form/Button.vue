446354fc6399d7e87c25e78e952276be6da908c7
LJ5O <75009579+LJ5O@users.noreply.github.com>
2025-06-11 11:10:31 +0200

    Login form CSS ready
    Now time to build the logic behind it

---
+++ apps/web-app/src/components/Form/Button.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+export interface Props{
+    content:string,
+    type?:'primary'|'secondary'|'danger'
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    type: 'primary'
+})
+
+const buttonClass:string = computed(()=>{
+    switch(props.type){
+        case 'secondary':
+            return 'bg-gray-500 hover:bg-gray-600 text-white'
+        case 'danger':
+            return 'bg-red-600 hover:bg-red-700 text-white'
+        case 'primary':
+        default:
+            return 'bg-green-600 hover:bg-green-700 text-white'
+    }
+});
+</script>
+
+<template>
+    <button
+        class="cursor-pointer w-fit pl-8 pr-8 pt-2 pb-2 border-1 border-neutral-300 rounded-xl"
+        :class="buttonClass"
+    >
+        {{content}}
+    </button>
+</template>
--- apps/web-app/src/views/Login.vue
+++ apps/web-app/src/views/Login.vue
@@ -2,16 +2,21 @@
     import {ref} from 'vue'
 
     import Input from "../components/Form/Input.vue"
+    import Button from "../components/Form/Button.vue"
 
     const login = ref<string>('')
     const password = ref<string>('')
+
+    function test(){
+        window.alert(login.value+'@'+password.value);
+    }
 </script>
 
 <template>
     <div class="w-full h-full flex justify-center content-center flex-wrap flex-col gap-4">
         <h1 class="text-white w-1/4 min-w-80 text-center text-2xl lg:text-5xl">Application</h1>
         <div class="
-            w-1/4 h-2/4 min-w-80 min-h-[400px] bg-blue-600 border-2 border-gray-500 rounded-lg shadow-2xl/80 shadow-black
+            w-1/4 h-fit min-w-80 bg-blue-600 border-2 border-gray-500 rounded-lg shadow-2xl/80 shadow-black
             flex flex-col content-center text-center
         ">
 
@@ -19,13 +24,15 @@
 
             <label class="login-input">
                 <h2>Login</h2>
-                <Input v-model="value" class="w-3/4 mr-auto ml-auto"/>
+                <Input v-model="login" class="w-3/4 mr-auto ml-auto"/>
             </label>
 
             <label class="login-input">
                 <h2>Password</h2>
-                <Input v-model="value" class="w-3/4 mr-auto ml-auto" type="password"/>
+                <Input v-model="password" class="w-3/4 mr-auto ml-auto" type="password"/>
             </label>
+
+            <Button content="Login" class="content-end my-3 self-end mr-5" @click="test()" />
         </div>
     </div>
 </template>
