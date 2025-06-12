ac33d365682a3d77cbf174a20f3325aa2655ac77
LJ5O <75009579+LJ5O@users.noreply.github.com>
2025-06-12 20:51:17 +0200

    Menu links background color

--- apps/web-app/src/components/PageTemplate/LeftMenu/LeftMenu.vue
+++ apps/web-app/src/components/PageTemplate/LeftMenu/LeftMenu.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-    import MenuElement from './MenuElement.vue'
-    /*import { Icon } from "@iconify/vue";
 
-    interface Props {
-        icon:string,
-        text:string
-    }
-    const props = defineProps<Props>();*/
+    import { useRoute } from 'vue-router'
+    import MenuElement from './MenuElement.vue'
+
+    const route = useRoute();
 
 </script>
 
@@ -15,7 +12,7 @@
 
         <div class="flex flex-col gap-2 mt-4 @container">
             <MenuElement icon="solar:dialog-2-line-duotone" text="Talk"/>
-            <MenuElement icon="solar:dialog-2-line-duotone" text="Talk" :currentTab="true"/>
+            <MenuElement icon="solar:dialog-2-line-duotone" text="Talk" :currentTab="route.name === 'control'"/>
             <MenuElement icon="solar:dialog-2-line-duotone" text="Talk"/>
         </div>
         
