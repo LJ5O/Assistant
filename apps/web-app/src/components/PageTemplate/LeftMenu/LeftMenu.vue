d7ff2fe1ec29c1b17f21b392bcf1048a4ebee5d0
LJ5O <75009579+LJ5O@users.noreply.github.com>
2025-06-26 14:58:04 +0200

    New red dots on menu

--- apps/web-app/src/components/PageTemplate/LeftMenu/LeftMenu.vue
+++ apps/web-app/src/components/PageTemplate/LeftMenu/LeftMenu.vue
@@ -11,7 +11,7 @@
     <div class="flex flex-col justify-between bg-neutral-800">
 
         <div class="flex flex-col gap-2 mt-4 @container">
-            <MenuElement icon="solar:dialog-2-line-duotone" text="Talk"/>
+            <MenuElement icon="solar:dialog-2-line-duotone" text="Talk" notif="true"/>
             <MenuElement icon="solar:dialog-2-line-duotone" text="Talk" :currentTab="route.name === 'control'"/>
             <MenuElement icon="solar:dialog-2-line-duotone" text="Talk"/>
         </div>
--- apps/web-app/src/components/PageTemplate/LeftMenu/MenuElement.vue
+++ apps/web-app/src/components/PageTemplate/LeftMenu/MenuElement.vue
@@ -4,10 +4,12 @@
     interface Props {
         icon:string,
         text:string,
-        currentTab?:boolean
+        currentTab?:boolean,
+        notif?:boolean
     }
     const props = withDefaults(defineProps<Props>(),{
-        currentTab:false
+        currentTab:false,
+        notif:false
     });
 
 </script>
@@ -15,9 +17,10 @@
 <template>
     <div class="
         flex flex-row gap-2 p-1.5 text-neutral-300 h-fit justify-start w-7/8 mx-auto hover:bg-neutral-500/50
-        border-neutral-600 border-b-2 border-b-solid rounded-lg
+        border-neutral-600 border-b-2 border-b-solid rounded-lg relative
     "
     :class="currentTab ? 'bg-neutral-700' : ''">
+        <div v-if="notif" class="absolute w-2 h-2 rounded-full bg-red-500 top-1 left-1"></div>
         <Icon :icon="icon" class="text-3xl"/>
         <p class="text-2xl hidden [@container(min-width:120px)]:block">{{text}}</p>
     </div>
